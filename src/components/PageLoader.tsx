7538b09636a041d69e2bf93cc76599d674a6c1de
BlacKlyExactly <supreme24d@gmail.com>
2021-07-26 14:26:36 +0200

    Pageloader Fix

--- src/components/PageLoader.tsx
+++ src/components/PageLoader.tsx
@@ -1,24 +1,32 @@
 import React, { FC, useState, useEffect, useRef } from "react";
 import gsap from "gsap"
-
 import styled from "styled-components";
+
 import Loader from "./Loader";
 
 const PageLoader: FC = () => {
     const [ isLoaded, setLoadedState ] = useState<boolean>(false);
 
     const loader = useRef<HTMLDivElement>(null);
+    const tl = useRef<GSAPTimeline>(gsap.timeline());
 
     useEffect(() => {
-        if(document.readyState === "complete"){
-            gsap.to(loader.current, { opacity: 0, duration: 1, ease: "expo.inOut" });
-            setTimeout(() => setLoadedState(true), 1000);
-        }
-    }, [ ])
+        const load = () => setLoadedState(true);
+        window.addEventListener("load", load);
 
-    if(isLoaded){
-        return null;
-    }
+        if(isLoaded){
+            tl.current.to(loader.current, { opacity: 0, delay: 0.5, duration: 0.5, ease: "power4.in" })
+                .set(loader.current, { zIndex: -1 });
+        }
+
+        setTimeout(()=> {
+            !isLoaded && setLoadedState(true)
+        }, 400)
+
+        return () => {
+            window.removeEventListener("load", load);
+        }
+    }, [ isLoaded ])
 
     return(
         <Wrapper ref={loader}>
