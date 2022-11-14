7f28e9a4c7c93dde9de2eb59109dbb5166611b9d
BlacKlyExactly <supreme24d@gmail.com>
2022-11-14 22:07:57 +0100

    Disable loader

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,27 +1,27 @@
-import React, { FC, ReactNode } from "react";
-import GlobalStyles from "../assets/styles/globals";
-import { Helmet } from "react-helmet";
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import React, { FC, ReactNode } from "react"
+import GlobalStyles from "../assets/styles/globals"
+import { Helmet } from "react-helmet"
+import { ToastContainer } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
 
-import Footer from "../components/Footer";
-import PageLoader from "../components/PageLoader";
+import Footer from "../components/Footer"
+import PageLoader from "../components/PageLoader"
 
 const Layout: FC<LayoutProps> = ({ children }: LayoutProps) => (
-    <>  
-        <Helmet>
-            <script src="https://www.google.com/recaptcha/api.js" async defer />
-        </Helmet>
-        <ToastContainer />
-        <GlobalStyles />
-        <PageLoader/>
-        {children}
-        <Footer/>
-    </>
+  <>
+    <Helmet>
+      <script src="https://www.google.com/recaptcha/api.js" async defer />
+    </Helmet>
+    <ToastContainer />
+    <GlobalStyles />
+    {/* <PageLoader/> */}
+    {children}
+    <Footer />
+  </>
 )
 
 type LayoutProps = {
-    children: ReactNode,
+  children: ReactNode
 }
 
-export default Layout;
+export default Layout
